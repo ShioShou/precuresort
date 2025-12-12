eb2677e64bf77a3af474d01e50c3027537b0f7ad
ShioShou <shioshoujo@gmail.com>
2025-12-12 05:23:11 -0600

    Update data.js
    fixing data(again)

--- src/js/data.js
+++ src/js/data.js
@@ -15,7 +15,7 @@ const dataSet = {};
  * 
  * @example '2018-02-20'
 */
-let dataSetVersion = '2025-12-11';
+let dataSetVersion = '2025-12-10';
 
 /**
  * Image root, will be appended to the start of every image URL.
